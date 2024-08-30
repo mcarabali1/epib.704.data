fcd61e26d83ff5a16a4e24140dd5c23a152e75f9
Edgar Ortiz-Brizuela <edgar.brizuela@hotmail.com>
2024-08-30 15:36:46 -0600

    ...

--- data-raw/load_data.R
+++ data-raw/load_data.R
@@ -11,7 +11,7 @@ births<-read.csv("data-raw/births.csv")
 sampledata<-read.csv("data-raw/sampledata.csv")
 
 # 4. sb.data1
-sb.data1<-read.csv("data-raw/sb.data1.csv")
+sbdata1<-read.csv("data-raw/sb.data1.csv")
 
 # 5. db_pd_sep22pm
 db_pd_sep22pm<-haven::read_dta("data-raw/db_pd_sep22pm.dta")
@@ -21,6 +21,6 @@ db_pd_sep22pm<-haven::read_dta("data-raw/db_pd_sep22pm.dta")
 usethis::use_data(plato,  
                   births,
                   sampledata,
-                  sb.data1,
+                  sbdata1,
                   db_pd_sep22pm,
                   overwrite = TRUE)
